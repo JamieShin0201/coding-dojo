9af75280e2db2cc46c2cf570df73f29169f0be23
JamieShin0201 <jamieshin1450@gmail.com>
2022-03-29 17:53:53 +0900

    상품 목록, 등록, 수정페이지 구현

---
+++ jpashop/src/main/java/me/jpashop/web/BookForm.java
@@ -0,0 +1,21 @@
+package me.jpashop.web;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class BookForm {
+
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    private int stockQuantity;
+
+    private String author;
+
+    private String isbn;
+}
