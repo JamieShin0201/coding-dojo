ff0bc0ba18ecb4a8d2488f94192cda120ec18147
JamieShin0201 <jamieshin1450@gmail.com>
2022-03-31 19:32:39 +0900

    JdbcTemplate 회원 저장소 구현

---
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,47 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@Transactional
+@SpringBootTest
+public class MemberServiceIntegrationTest {
+
+    @Autowired
+    MemberService memberService;
+
+    Member memberA;
+    Member memberB;
+
+    @BeforeEach
+    void setUp() {
+        memberA = Member.builder()
+                .name("memberA")
+                .build();
+
+        memberB = Member.builder()
+                .name("memberB")
+                .build();
+    }
+
+    @Test
+    void 회원가입() {
+        memberService.join(memberA);
+
+        assertThat(memberA.getId()).isNotNull();
+    }
+
+    @Test
+    void 중복_회원_예외() {
+        memberService.join(memberA);
+
+        assertThrows(IllegalStateException.class, () -> memberService.join(memberA));
+    }
+}
