83453dc29b71f7c9ef5d9ad2247417738bda3a0f
JamieShin0201 <jamieshin1450@gmail.com>
2022-03-31 19:24:02 +0900

    Jdbc 회원 저장소 구현

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,6 +4,7 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,6 +40,7 @@ public class MemberController {
         return "members/memberList";
     }
 
+    @Setter
     @Getter
     static class MemberForm {
         private String name;
