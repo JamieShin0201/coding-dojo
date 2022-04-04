ae3d8baa4a6eb7e80f95addf768b1d1d10e9e9c6
JamieShin0201 <jamieshin1450@gmail.com>
2022-04-04 17:48:37 +0900

    Xml 로 빈 설정

---
+++ core/src/test/java/me/core/xml/XmlAppContext.java
@@ -0,0 +1,19 @@
+package me.core.xml;
+
+import me.core.service.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("AppConfig.xml");
+
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
