93f67164a76295bd46d279fe10cfbce034928043
JamieShin0201 <jamieshin1450@gmail.com>
2022-05-08 23:20:38 +0900

    필드 동기화 - 적용

---
+++ advanced/src/main/java/me/jamie/advanced/LogTraceConfig.java
@@ -0,0 +1,15 @@
+package me.jamie.advanced;
+
+import me.jamie.advanced.trace.logtrace.FieldLogTrace;
+import me.jamie.advanced.trace.logtrace.LogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LogTraceConfig {
+
+    @Bean
+    public LogTrace logTrace() {
+        return new FieldLogTrace();
+    }
+}
---
+++ advanced/src/main/java/me/jamie/advanced/v3/OrderRepositoryV3.java
@@ -0,0 +1,36 @@
+package me.jamie.advanced.v3;
+
+import lombok.RequiredArgsConstructor;
+import me.jamie.advanced.trace.TraceStatus;
+import me.jamie.advanced.trace.logtrace.LogTrace;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class OrderRepositoryV3 {
+
+    private final LogTrace trace;
+
+    public void save(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderRepository.save()");
+            if (itemId.equals("ex")) {
+                throw new IllegalStateException("예외 발생");
+            }
+            sleep(1000);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
